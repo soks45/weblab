803fb29744f65cb87392265f87b44a743428eb49
soks45 <65854276+soks45@users.noreply.github.com>
2021-12-19 18:14:57 +0500

    Create ChangeThemeButton.js

---
+++ script/ChangeThemeButton.js
@@ -0,0 +1,7 @@
+let buttonTEMA = document.getElementById('buttonTEMAid');
+buttonTEMA.addEventListener("click", ChangeTheme);
+
+function ChangeTheme()
+{
+  document.body.style.background = "#d2d43e";
+}
