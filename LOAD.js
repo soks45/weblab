0884758f2f6848f2c76581d480fb7e189b1d640a
soks45 <65854276+soks45@users.noreply.github.com>
2021-12-19 18:18:47 +0500

    Delete LOAD.js

--- LOAD.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", ready);
-function ready() {
-  alert('Сайт успешно загружен');
-}
-
-window.onbeforeunload = function() {
-  return "Есть несохранённые изменения. Всё равно уходим?";
-};
